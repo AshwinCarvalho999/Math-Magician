89f7fc4178a22cf83eaa0b201af29eb81f214360
Ashwin Carvalho <ashwincarvalho27@gmail.com>
2022-10-27 17:22:35 +0530

    Implementation of react hooks

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Calculator from './components/Calculator';
+import Home from './components/pages/Home';
+import Quotes from './components/pages/Quotes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React task
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="Calculator" element={<Calculator />} />
+          <Route path="Quotes" element={<Quotes />} />
+        </Routes>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
